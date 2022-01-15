1da275818e8e688641f3a8830d311df7c4bf87a0
Kim JaeMin <whlthd1207@gmail.com>
2022-01-15 19:28:56 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@
 import logo from './instagram.png';
 import fontLogo from './Instagram_logo2.png';
 import facebookLogo from './facebook_logo.png';
+import Customer from './components/Customer';
 import './App.css';
+import React from 'react';
+import { Component } from 'react/cjs/react.production.min';
 
+/*
 function App() {
   return (
+    //<Customer/>
     <div className="App">
       <div className="article_loginMenu">
         <div className="article_loginImage">
@@ -41,5 +46,58 @@ function App() {
     </div>
   );
 }
+*/
+
+const customers = [
+  {
+    'id' : 1,
+    'image' : 'https://placeimg.com/64/64/any',
+    'name' : '김재민',
+    'birthday' : '931207',
+    'gender' : '남자',
+    'job' : '개발자'
+  },
+  {
+    'id' : 2,
+    'image' : 'https://placeimg.com/64/64/2',
+    'name' : '유재석',
+    'birthday' : '701210',
+    'gender' : '남자',
+    'job' : '방송인'
+  },
+  {
+    'id' : 3,
+    'image' : 'https://placeimg.com/64/64/3',
+    'name' : '강호동',
+    'birthday' : '650513',
+    'gender' : '남자',
+    'job' : '방송인'
+  }
+
+]
+
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        {
+          customers.map( c =>{
+            return (<Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              birthday={c.name}
+              gender={c.gender}
+              job={c.job}
+            />);
+          })
+        }
+      </div>  
+    );
+  }
+}
+
+
 
 export default App;
